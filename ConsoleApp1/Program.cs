0c40e5d8fa0954803002264f182232712894a286
Matt Burke <matt.burke@viagiotech.com>
2023-06-14 13:34:56 -0500

    add ms dependency injection

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,11 +1,40 @@
 ﻿
 // SendTestPushNotification depends on an IPushNotificationService, so we will provide
 // the google one. Maybe someone will need the apple one later
-var tester = new SendTestPushNotification(new GooglePushNotificationService());
-tester.Send();
+
+using System.Diagnostics;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+
+// var tester = new SendTestPushNotification(new GooglePushNotificationService());
+// tester.Send();
+
+
+var services = new ServiceCollection();
+services.AddPushNotifications();
+
+
+var serviceProvider = services.BuildServiceProvider();
+
+// beginning of http request
+var httpRequestSCope = serviceProvider.CreateScope();
+httpRequestSCope.ServiceProvider.GetRequiredService<SendTestPushNotification>();
+httpRequestSCope.ServiceProvider.GetRequiredService<SendTestPushNotification>();
+
+// end of http request
+httpRequestSCope.Dispose();
+
 
 
 // ----
+
+public static class ServiceCollectionExtensions {
+    public static void AddPushNotifications(this ServiceCollection services)
+    {
+        services.AddScoped<SendTestPushNotification>();
+        services.AddScoped<IPushNotificationService, GooglePushNotificationService>();
+    }
+}
 
 public interface IPushNotificationService
 {
@@ -18,7 +47,7 @@ public class GooglePushNotificationService : IPushNotificationService
     public void SendNotification(string notification)
     {
         // talk to push.google.com
-        throw new NotImplementedException();
+        Console.WriteLine("SEnding a push notification of \"{0}\" to google", notification);
     }
 
     public bool IsConnected()
@@ -42,12 +71,13 @@ class ApplePushNotificationService : IPushNotificationService
 }
 
 
-public class SendTestPushNotification
+public class SendTestPushNotification: IDisposable
 {
     private readonly IPushNotificationService _pushNotificationService;
 
     public SendTestPushNotification(IPushNotificationService pushNotificationService)
     {
+        Console.WriteLine("Creating SendTestPushNotification with a {0}", pushNotificationService.GetType().Name);
         _pushNotificationService = pushNotificationService;
     }
 
@@ -55,5 +85,9 @@ public class SendTestPushNotification
     {
         _pushNotificationService.SendNotification("this is a test");
     }
-    
+
+    public void Dispose()
+    {
+        Console.WriteLine("cleaning up!");
+    }
 }
